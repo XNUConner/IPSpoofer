b8f5f7533f82f05c504e70852670862a10d1050b
XNUConner <72773002+XNUConner@users.noreply.github.com>
2021-03-29 03:47:41 -0500

    Included necessary headers and defined main() and signalCatch()

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netinet/ip.h>
+#include<netinet/udp.h>
+#include<arpa/inet.h>
+
+void signalCatch(void);
+
+int main(void) {
+    return 0;
+}
